c659295cf8d3b580c41e6b8b4832635e25945eb2
twkimfe <arthurkim135@naver.com>
2024-10-30 17:17:20 +0900

    feat: 3.5강 강의 내용 실습

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,5 +1,10 @@
 import mul, { add, sub } from "./math.js";
 
-console.log(add(1, 2));
-console.log(sub(1, 2));
-console.log(mul(2, 3));
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
+
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(mul(2, 3));
