6ac12425c2779757081a97d76c72c3812afe3d65
twkimfe <arthurkim135@naver.com>
2024-11-04 17:16:56 +0900

    feat: 5.9강 강의 내용 실습

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,31 +1,12 @@
 import './App.css'
-import { useState } from 'react';
+import Register from './components/Register';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [light, setLight] = useState('OFF');
-
   return (
     <>
-      <div>
-        <h1>{light}</h1>
-        <button onClick={() => {
-          setLight(light === 'ON' ? 'OFF' : 'ON')
-        }}
-        >{light === "ON" ? "끄기" : "켜기"}</button>
-      </div>
-      <div>
-        <h1>{count}</h1>
-        <button
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          +
-        </button>
-      </div>
+      <Register />
     </>
-  )
+  );
 }
 
 export default App;
