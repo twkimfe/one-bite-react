c72668975216391844b9265d13453ef20f21497b
twkimfe <arthurkim135@naver.com>
2024-11-10 16:42:52 +0900

    feat: 10.4강 강의 내용 실습

--- section10/src/App.jsx
+++ section10/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { useRef, useState, useReducer } from 'react'
+import { useRef, useState, useReducer, useCallback } from 'react'
 import Header from './components/Header'
 import Editor from './components/Editor'
 import List from './components/List';
@@ -46,7 +46,7 @@ function App() {
   const [todos, dispatch] = useReducer(reducer, mockData);
   const idRef = useRef(3)
 
-  const onCreate = (content) => {
+  const onCreate = useCallback((content) => {
     dispatch({
       type: "CREATE",
       data: {
@@ -56,21 +56,21 @@ function App() {
         date: new Date().getTime(),
       }
     });
-  };
+  }, []);
 
-  const onUpdate = (targetId) => {
+  const onUpdate = useCallback((targetId) => {
     dispatch({
       type: "UPDATE",
       targetId: targetId
     });
-  };
+  }, []);
 
-  const onDelete = (targetId) => {
+  const onDelete = useCallback((targetId) => {
     dispatch({
       type: "DELETE",
       targetId: targetId,
     });
-  };
+  }, []);
 
   return (
     <div className='App'>
@@ -85,4 +85,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
