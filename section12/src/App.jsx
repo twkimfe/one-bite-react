0ff541a3b231a33c3143a153a85488408ccb1c51
twkimfe <arthurkim135@naver.com>
2024-11-15 14:54:36 +0900

    feat: 12.7강 강의 내용 실습

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,22 +1,51 @@
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Link, useNavigate } from 'react-router-dom'
 import Home from './pages/Home'
 import Diary from './pages/Diary'
 import New from './pages/New'
 import Notfound from './pages/Notfound'
-
+import Button from './components/Button'
+import Header from './components/Header'
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
+
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/new' element={<New />} />
-      <Route path='/diary' element={<Diary />} />
-      <Route path='/*' element={<Notfound />} />
-    </Routes>
+    <>
+      <Header title={"Header"}
+        leftChild={<Button text={"left"} />}
+        rightChild={<Button text={"right"} />}
+      />
+      <Button
+        text={"123"}
+        onClick={() => {
+          console.log("클릭 됨");
+        }}
+      />
+      <Button
+        text={"123"}
+        type={"POSITIVE"}
+        onClick={() => {
+          console.log("클릭 됨");
+        }}
+      />
+      <Button
+        text={"123"}
+        type={"NEGATIVE"}
+        onClick={() => {
+          console.log("클릭 됨");
+        }}
+      />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/new' element={<New />} />
+        <Route path='/diary/:id' element={<Diary />} />
+        <Route path='/*' element={<Notfound />} />
+      </Routes>
+    </>
   )
 }
 
--- section12/src/pages/Diary.jsx
+++ section12/src/pages/Diary.jsx
@@ -1,4 +1,9 @@
+import { useParams } from "react-router-dom";
+
 const Diary = () => {
+  const params = useParams();
+  console.log(params);
+
   return <div>Diary</div>
 };
 
