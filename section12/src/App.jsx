3bf3ac91638f1157f729ad59f244995b6337c99f
twkimfe <arthurkim135@naver.com>
2024-11-16 12:15:33 +0900

    feat: 12.8강 강의 내용 실습

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,48 +1,41 @@
 import './App.css'
+import { useReducer } from 'react'
 import { Routes, Route, Link, useNavigate } from 'react-router-dom'
 import Home from './pages/Home'
 import Diary from './pages/Diary'
 import New from './pages/New'
+import Edit from './pages/Edit'
 import Notfound from './pages/Notfound'
-import Button from './components/Button'
-import Header from './components/Header'
 
-// 1. "/" : 모든 일기를 조회하는 Home 페이지
-// 2. "/new" : 새 일기를 작성하는 New 페이지
-// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: '1번 일기 내용',
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: '2번 일기 내용',
+  },
+];
+
+function reducer(state, action) {
+  return state;
+}
+
 function App() {
+  const [data, dispatch] = useReducer(reducer, mockData);
 
   return (
     <>
-      <Header title={"Header"}
-        leftChild={<Button text={"left"} />}
-        rightChild={<Button text={"right"} />}
-      />
-      <Button
-        text={"123"}
-        onClick={() => {
-          console.log("클릭 됨");
-        }}
-      />
-      <Button
-        text={"123"}
-        type={"POSITIVE"}
-        onClick={() => {
-          console.log("클릭 됨");
-        }}
-      />
-      <Button
-        text={"123"}
-        type={"NEGATIVE"}
-        onClick={() => {
-          console.log("클릭 됨");
-        }}
-      />
-
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/new' element={<New />} />
         <Route path='/diary/:id' element={<Diary />} />
+        <Route path='/edit/:id' element={<Edit />} />
         <Route path='/*' element={<Notfound />} />
       </Routes>
     </>
